db5c07ef63ee7be3c19927d7609a6863fd46b078
Maycon Carlete <maycon.carlete@gmail.com>
2021-08-04 20:45:07 -0300

    feat: hello world project aws ebgit config

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message:'hello world'})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`We are online ${PORT}`)
+})
